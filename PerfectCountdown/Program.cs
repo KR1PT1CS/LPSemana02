50e89d5210064cd250f121b110d6b8fe581dfb23
KR1PT1CS <mogos2625727@gmail.com>
2025-02-28 14:34:06 +0000

    updating the main branch since the visual studio code had not been saved. Added the rest of the code, rewrote some lines of code and identation errors (all commits were overwritten, this one is the equivalent of 3 commits)

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Net;
 
 namespace PerfectCountdown
 {
-    public class Program
+     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
-        }
+            while (true){
+            
+                 Console.Write("Start Number: ");
+                 int.TryParse(Console.ReadLine(), out int start);
+                 Console.Write("Step Number: ");
+                 int.TryParse(Console.ReadLine(), out int step)
+
+                 if (start <= 1){
+                     Console.WriteLine("Out-of-range start ou step. Try again");
+                     continue;
+                 }
+                 else if (step >= start){
+                    Console.WriteLine("Start must be higher than step. Try again");
+                    continue;
+                 }
+                 else if (start % step != 0){
+                    Console.WriteLine("Start not divisible by step. Try again");
+                    continue;
+                 }
+                 while (start != 0){
+                    Console.WriteLine(start);
+                    break;
+                 }
+            }
+        } 
     }
 }
