0d431fd176487500f98c9669bb4ee9befad1bee3
KR1PT1CS <mogos2625727@gmail.com>
2025-02-28 13:16:39 +0000

    Created Cilindro, added it to the solution and gave it height & radius

---
+++ Cilindro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+            Console.Write("Altura do cilindro: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Raio do cilindro: ");
+            double r = double.Parse(Console.ReadLine());
+        }
+    }
+}
