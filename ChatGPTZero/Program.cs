b8e442183bb3194ecf9d77a97ba22fac3fc84b3b
KR1PT1CS <mogos2625727@gmail.com>
2025-02-28 13:28:06 +0000

    Added a question to ChatGPTZero

--- ChatGPTZero/Program.cs
+++ ChatGPTZero/Program.cs
@@ -17,17 +17,14 @@ namespace ChatGPTZero
             // resposta adequada
             switch (question)
             {
-                case "How are you?":
-                    response = "I'm fine, thank you";
-                    break;
                 case "What's your name?":
-                    response = "Gran'pa";
+                    response = "William Ritcher";
+                    break;
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
                     break;
                 case "What's your mission?":
                     response = "Destroy mankind!";
-                    break;
-                default:
-                    response = "You got me, I'm not THAT smart!";
                     break;
             }
 
