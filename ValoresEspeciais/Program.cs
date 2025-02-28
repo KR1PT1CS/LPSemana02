35bda7df7cf2d41e410862539734778f26402935
KR1PT1CS <mogos2625727@gmail.com>
2025-02-28 13:23:37 +0000

    Added Overflow & Underflow float s

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -56,20 +56,18 @@ namespace ValoresEspeciais
             decimal dmmin = decimal.MinValue;
             Console.WriteLine($"decimal máximo: {dmmax:F3}");
             Console.WriteLine($"decimal mínimo: {dmmin:F3}");
-
-            float fminfinite = float.NegativeInfinity;
-            float fmaxinfinite = float.PositiveInfinity;
-            float fnan = float.NaN;
-            Console.WriteLine($"float mínimo infinito: {fminfinite}");
-            Console.WriteLine($"float máximo infinito: {fmaxinfinite}");
-            Console.WriteLine($"float NaN: {fnan}");
-
-            double dminfinite = double.NegativeInfinity;
-            double dmaxinfinite = double.PositiveInfinity;
-            double dnan = double.NaN;
-            Console.WriteLine($"double mínimo infinito: {dminfinite}");
-            Console.WriteLine($"double máximo infinito: {dmaxinfinite}");
-            Console.WriteLine($"double NaN: {dnan}");
+            float fmin = float.MinValue;
+            float overflowFloat = fmax * 10.0f;
+            float overflowInfinity = fmax * fmax;
+            float underflowFloat = fmin / 1000000.0f;
+            Console.WriteLine($"float máximo: {fmax:F3}");
+            Console.WriteLine($"float máx * 10.0f overflow: {overflowFloat:F3}");
+            Console.WriteLine($"float máx * máx overflow: {overflowInfinity:F3}");
+            Console.WriteLine($"float mínimo: {fmin:F3}");
+            Console.WriteLine($"float mínimo / 1000000.0f underflow: {underflowFloat:F3}");
+            double dmax = double.MaxValue;
+            double dmin = double.MinValue;
+            Console.WriteLine($"double máximo: {dmax:F3}");
         }
     }
 }
